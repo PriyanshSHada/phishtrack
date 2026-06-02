0420ce98c2014da7e9269517e2fa44287ec0f6e4
Priyansh Singh Hada <priyanshsinghhada@gmail.com>
2026-06-02 20:47:12 +0530

    Update Android BASE_URL to use live Render deployment

--- PhishTrack/app/src/main/java/com/example/phishtrack/di/AppModule.kt
+++ PhishTrack/app/src/main/java/com/example/phishtrack/di/AppModule.kt
@@ -22,7 +22,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://phishtrack.onrender.com/"
 
     @Provides
     @Singleton
